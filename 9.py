47e69f4b7e55d6beb48cc7b1ba5d6cbf8113ebc6
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-08 22:17:28 +0300

    Improve GenerateFio function

--- 9.py
+++ 9.py
@@ -1,15 +1,20 @@
 import openpyxl
 from russian_names import RussianNames
 
-def generateFIO():
-    fio = RussianNames().get_person()
-    tuple_fio = tuple(fio.split())
+def generateFIO(amount:int) -> list:
+    fio_list = []
+    fio = ''
+    for i in range(amount):
+        fio = str(i + 1) + ' ' + RussianNames().get_person()
+        fio_list.append(tuple(fio.split()))
     print(fio)
-    print(tuple_fio)
-    print(type(fio))
+    print(fio_list)
+    return fio_list
+
 
 def main():
-    generateFIO()
+    input_number = int(input('Enter a number - '))
+    generateFIO(input_number)
 
 if __name__ == "__main__":
     main()
