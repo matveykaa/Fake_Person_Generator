931baa9218df2782c36019bc8e61d6cbc6c8b8cb
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-09 23:34:48 +0300

    Add opportunity to add info to existing table

--- 9.py
+++ 9.py
@@ -1,4 +1,5 @@
 import openpyxl
+from openpyxl.reader.excel import load_workbook
 from russian_names import RussianNames
 
 def generateFIO(amount:int) -> list:
@@ -12,20 +13,24 @@ def generateFIO(amount:int) -> list:
     return fio_list
 
 def writeToXlsx(write_list: list):
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    sheet.title = "People"
-
-    sheet['A1'] = "Номер"
-    sheet['B1'] = "Фамилия"
-    sheet['C1'] = "Имя"
-    sheet['D1'] = "Отчество"
+    try:
+        wb = load_workbook('people.xlsx')
+        sheet = wb.active
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        sheet.title = "People"
+        sheet['A1'] = "Номер"
+        sheet['B1'] = "Фамилия"
+        sheet['C1'] = "Имя"
+        sheet['D1'] = "Отчество"
+    last_row = sheet.max_row
 
     for i, fio in enumerate(write_list):
-        sheet.cell(row=i + 2, column=1).value = fio[0]
-        sheet.cell(row=i + 2, column=2).value = fio[3]
-        sheet.cell(row=i + 2, column=3).value = fio[1]
-        sheet.cell(row=i + 2, column=4).value = fio[2]
+        sheet.cell(row=last_row + i + 1, column=1).value = last_row + i
+        sheet.cell(row=last_row + i + 1, column=2).value = fio[3]
+        sheet.cell(row=last_row + i + 1, column=3).value = fio[1]
+        sheet.cell(row=last_row + i + 1, column=4).value = fio[2]
     wb.save('people.xlsx')
 
 def main():
