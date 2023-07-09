762235258dde922e2013753984bfdda5e4845e7c
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-09 23:58:31 +0300

    Add opportunity to choose create a new table or add info to existing file

--- 9.py
+++ 9.py
@@ -12,31 +12,48 @@ def generateFIO(amount:int) -> list:
     print(fio_list)
     return fio_list
 
-def writeToXlsx(write_list: list):
-    try:
+def clearWorksheet(sheet):
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
+        for cell in row:
+            cell.value = None
+
+def writeToXlsx(write_list: list, ans: str):
+    if ans == 'N':
         wb = load_workbook('people.xlsx')
         sheet = wb.active
-    except FileNotFoundError:
-        wb = openpyxl.Workbook()
-        sheet = wb.active
+        clearWorksheet(sheet)
         sheet.title = "People"
-        sheet['A1'] = "Номер"
-        sheet['B1'] = "Фамилия"
-        sheet['C1'] = "Имя"
-        sheet['D1'] = "Отчество"
-    last_row = sheet.max_row
-
+        last_row = 1
+    elif ans == 'Y':
+        try:
+            wb = load_workbook('people.xlsx')
+            sheet = wb.active
+            last_row = sheet.max_row
+        except FileNotFoundError:
+            wb = openpyxl.Workbook()
+            sheet = wb.active
+            sheet.title = "People"
+            last_row = 1
+    else:
+        print('Error: Write N or Y only')
+        return
+    sheet['A1'] = "Номер"
+    sheet['B1'] = "Фамилия"
+    sheet['C1'] = "Имя"
+    sheet['D1'] = "Отчество"
     for i, fio in enumerate(write_list):
-        sheet.cell(row=last_row + i + 1, column=1).value = last_row + i
-        sheet.cell(row=last_row + i + 1, column=2).value = fio[3]
-        sheet.cell(row=last_row + i + 1, column=3).value = fio[1]
-        sheet.cell(row=last_row + i + 1, column=4).value = fio[2]
+            sheet.cell(row=last_row + i + 1, column=1).value = last_row + i
+            sheet.cell(row=last_row + i + 1, column=2).value = fio[3]
+            sheet.cell(row=last_row + i + 1, column=3).value = fio[1]
+            sheet.cell(row=last_row + i + 1, column=4).value = fio[2]
     wb.save('people.xlsx')
+
 
 def main():
     input_number = int(input('Enter a number: '))
+    answer = input('Do you want to add FIOs to existing file? (N - no, Y - yes): ')
     fio_list = generateFIO(input_number)
-    writeToXlsx(fio_list)
+    writeToXlsx(fio_list, answer)
 
 if __name__ == "__main__":
     main()
